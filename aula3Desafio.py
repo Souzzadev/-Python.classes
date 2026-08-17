5cfc56705e4b8dc4de34b88e2c7ac2f6915cebd4
Souzzadev <souzzadev@gmail.com>
2026-08-16 23:00:04 -0300

    Python coding challenges

---
+++ aula3Desafio.py
@@ -0,0 +1,40 @@
+# desafio 1 Crie um programa que mostre no terminal:
+print("==========================")
+print("       MEU PERFIL")
+print("==========================")
+print("Nome: Mariana")
+print("Profissão: Analista de Sistemas")
+print("Linguagem: Python")
+print("Status: Iniciando meus estudos")
+print("==========================")
+
+# desafio 2 Faça o programa imprimir exatamente uma frase apresentando você.
+print ("Olá, Meu nome é Mariana e estou começando minha jornada com Python.")
+
+# desafio 3 Crie uma pequena mensagem de apresentação:
+
+print("Olá\nMeu nome é Mariana. \nEstou estudando Python. \nMeu objetivo é aprender programação e criar meus próprios projetos.")
+
+# desafio 4 Crie uma variável chamada:
+
+nome = "Mariana"
+
+print("Meu nome é "+ nome + "\nEstou estudando Python\nPython será minha nova habilidade.")
+
+# desafio 5 Sem executar primeiro, tente prever o que cada print() vai mostrar:
+
+"""
+print("Python")
+print("Python" "é" "legal")
+print("Python", "é", "legal")
+print("Olá\nMariana")
+"""
+
+"""
+vai aparecer assim:
+Python
+error
+Python é legal
+Olá
+Mariana
+"""
